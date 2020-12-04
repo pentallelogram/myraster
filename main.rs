aa2c3b74f33d66bcfa4172f984ae14ee0612f815
alewis <ahlewis90@gmail.com>
2020-12-04 21:20:16 +0000

    get ↖️  vectors working with point swapping

--- main.rs
+++ main.rs
@@ -3,19 +3,59 @@
 //    1. actually set pixel on image (need to make classes in rust)
 //    2. make it not need to go from top left to bottom right
 //    3. make it not only write 3 points when passing (1,1,4,80) for example
+use std::mem::swap;
 
 // Bresenham’s Line Drawing Algorithm
-fn line(x0: u32, y0: u32, x1: u32, y1: u32) {
+fn line(mut x0: u32, mut y0: u32, mut  x1: u32, mut y1: u32) {
 
-    // make 100 points between (x0, y0) and (x1, y1)
-    let len = x1-x0;
-    for n in x0..x1 {
 
-        let x: u32 = x0 + (x1-x0) * n * 1/len;
-        let y: u32 = y0 + (y1-y0) * n * 1/len;
+    let mut steep: bool = false;
+    let sx0: i64 = x0.into();
+    let sy0: i64 = y0.into();
+    let sx1: i64 = x1.into();
+    let sy1: i64 = y1.into();
+    let xdist = (sx0-sx1).abs();
+    let ydist = (sy0-sy1).abs();
+    if ydist > xdist {
+        steep = true;
+    }
+    println!("xdist: {}  {} ydist: {} so line is{} steep",
+        xdist, if steep { "<" } else { ">"}, 
+        ydist, if steep { "" } else {"n't"});
+    let looplen: u32 = if steep { ydist as u32 }
+                    else { xdist as u32 };
+    println!("looplen: {} (number of pixels to color)", looplen);
 
-        println!("{}:\t{}\t{}", n, x, y);
+/* BUG: my transpose isn't working when vectors point ↗️  or ↙️ .
+    if (x0 > x1 && y0 < y1) || (x1 > x0 && y1 < y0) {
+        println!("transpose!");
+        println!("x0: {}\ty0: {} \t&& \tx1: {}\ty1: {}", x0, y0, x1, y1);
+        swap(&mut x0, &mut y0);
+        swap(&mut x1, &mut y1);
+        println!("x0: {}\ty0: {} \t&& \tx1: {}\ty1: {}", x0, y0, x1, y1);
+        transpose = true;
+    }
+*/
 
+    if x0 > x1 {
+        println!("swap points!");
+        println!("x0: {}\ty0: {} \t&& \tx1: {}\ty1: {}", x0, y0, x1, y1);
+        swap(&mut x0, &mut x1);
+        swap(&mut y0, &mut y1);
+        println!("x0: {}\ty0: {} \t&& \tx1: {}\ty1: {}", x0, y0, x1, y1);
+    }
+
+
+    for n in 0..looplen {
+
+        let x: u32 = x0 + (x1-x0) * n * 1/looplen;
+        let y: u32 = y0 + (y1-y0) * n * 1/looplen;
+
+        if steep {
+            println!("{}:\t{}\t{}", n, y, x);
+        } else {
+            println!("{}:\t{}\t{}", n, x, y);
+        }
         // set pixel at (x,y) to color
     }
 
@@ -23,6 +63,6 @@ fn line(x0: u32, y0: u32, x1: u32, y1: u32) {
 }
 
 fn main() {
-    line( 1, 1, 4, 80);
-    println!("hello world");
+    line( 0, 40, 20, 20);
+    return;
 }
