4393235b4189d36a3ffda26f19ee316b7cbec149
alewis <ahlewis90@gmail.com>
2020-12-10 01:21:53 +0000

    readme

---
+++ README.md
@@ -0,0 +1,24 @@
+
+
+### todo
+
+- [x] draw a line
+- [ ] dammit I need to actuall learn RUST to implement this in Rust
+  - [x] rust-101 00/01
+  - [ ] rust-101 02/03
+  - [ ] rust-101 04/05
+  - [ ] rust-101 06/07
+  - [ ] rust-101 08/09
+  - [ ] rust-101 10/11
+  - [ ] rust-101 12/13
+  - [ ] rust-101 14/15
+  - [ ] rust-101 16
+- [ ] allow working with TGA files
+  - [ ] constructor / destructor
+  - [ ] `load_rle_data`
+  - [ ] `read_tga_file`
+  - [ ] `write_tga_file`
+  - [ ] `unload_rle_data`
+  - [ ] get/set
+  - [ ] flip horizontal / vertical
+  - [ ] scale
---
+++ src/main.rs
@@ -0,0 +1,63 @@
+// bug when x1 and x0 are same, loop won't exec (and would div by 0 if it did)
+// todo 
+//    1. actually set pixel on image (need to make classes in rust)
+use std::mem::swap;
+use "tgaimage.rs"
+//use std::{thread, time};
+
+// Bresenham’s Line Drawing Algorithm
+fn line( x0: u32,  y0: u32,   x1: u32,  y1: u32) {
+
+    //println!("\ncase: x0: {}\ty0: {} \t&& \tx1: {}\ty1: {}", x0, y0, x1, y1);
+
+    let mut steep: bool = false;
+    let mut sx0: i64 = x0.into();
+    let mut sy0: i64 = y0.into();
+    let mut sx1: i64 = x1.into();
+    let mut sy1: i64 = y1.into();
+    let xdist = (sx0-sx1).abs();
+    let ydist = (sy0-sy1).abs();
+    if ydist > xdist {
+        steep = true;
+    }
+
+    //println!("xdist: {}  {} ydist: {} so line is{} steep",
+    //    xdist, if steep { "<" } else { ">"}, 
+    //    ydist, if steep { "" } else {"n't"});
+    let looplen: i64 = if steep { ydist } else { xdist };
+
+    if y0 > y1 {
+        // make all ray's point down or sideways not up
+        //println!("swap points!");
+        //println!("x0: {}\ty0: {} \t&& \tx1: {}\ty1: {}", sx0, sy0, sx1, sy1);
+        swap(&mut sx0, &mut sx1);
+        swap(&mut sy0, &mut sy1);
+        //println!("x0: {}\ty0: {} \t&& \tx1: {}\ty1: {}", sx0, sy0, sx1, sy1);
+    }
+
+    for n in 0..looplen {
+        //println!("y0:{} y1:{}", y0, y1);
+        let _sx: i64 = sx0 + (sx1-sx0) * n /looplen;
+        let _sy: i64 = sy0 + (sy1-sy0) * n /looplen;
+        //println!("{}:\t{}\t{}", n, sx, sy);
+        // set pixel at (x,y) to color
+    }
+
+    return;
+}
+
+fn main() {
+
+    line( 0, 0, 20, 20);
+    line( 40, 40, 20, 20);
+    line( 40, 0, 20, 20);
+    line( 0, 40, 20, 20);
+
+    line( 20, 0, 20, 20);
+    line( 20, 40, 20, 20);
+    line( 40, 20, 20, 20);
+    line( 0, 20, 20, 20);
+    
+
+    return;
+}
